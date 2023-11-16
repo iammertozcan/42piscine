5fc738f9ca54c3eef4be8d0f9805a18aa1e3183c
Mert Özcan <130929421+mxrtxzcxn@users.noreply.github.com>
2023-11-16 13:29:03 +0300

    c03 added!

---
+++ c02/ex01/ft_strncpy.c
@@ -0,0 +1,17 @@
+char	*ft_strcpy(char *dest, char *src, unsigned int n)
+{
+	insigned int	i;
+
+	i = 0;
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
---
+++ c02/ex08/ft_strlowcase.c
@@ -0,0 +1,13 @@
+int	*ft_strlowcase(char *str)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if ((str[i] >= 'A') && (str[i] <= 'Z'))
+			str[i] += 32;
+		i++;
+	}
+	return (str);
+}
