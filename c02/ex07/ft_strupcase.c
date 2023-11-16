ae766258f5c41c3505715b4140e6c5c9754a1da5
Mert Özcan <130929421+mxrtxzcxn@users.noreply.github.com>
2023-11-16 13:32:32 +0300

    c02 added!

---
+++ c02/ex07/ft_strupcase.c
@@ -0,0 +1,13 @@
+char	*strupcase(char *str)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if ((str[i] >= 'a') && (str[i] <= 'z'))
+			str[i] = str[i] - 32;
+		i++;
+	}
+	return (str);
+}
