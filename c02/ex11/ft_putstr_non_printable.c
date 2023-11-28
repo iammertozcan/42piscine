9fc3af779a00165b0d4dae56710d6853ba3ac9b8
Mert Özcan <130929421+mxrtxzcxn@users.noreply.github.com>
2023-11-28 13:51:41 +0300

    ex11 added!

---
+++ c02/ex11/ft_putstr_non_printable.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr_non_printable(char *str)
+{
+	unsigned int	i;
+	char	*hex;
+
+	i = 0;
+	hex = "0123456789abcdef";
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 32 && str[i] <= 126)
+			ft_putchar(str[i]);
+		else
+		{
+			ft_putchar('\\');
+			ft_putchar(hex[str[i] / 16]);
+			ft_putchar(hex[str[i] % 16]);
+		}
+		i++;
+	}
+}
