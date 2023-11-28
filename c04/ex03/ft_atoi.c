e2b300e9219752c1307b31af480405f123374ed2
Mert Özcan <130929421+mxrtxzcxn@users.noreply.github.com>
2023-11-28 17:18:01 +0300

    ex04 added!

---
+++ c04/ex03/ft_atoi.c
@@ -0,0 +1,24 @@
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	size;
+	int	result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while ((str[i] >= '\t' && str[i] <= '\r') || str[i] == ' ')
+		i++;
+	while (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			sign = sign * 1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = (str[i] - '0') + (result * 10);
+		i++;
+	}
+	return (result * sign);
+}
