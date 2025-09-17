6d7636636a1459896e3d236ab6bb96ce19334a28
Mert Özcan <iammertozcan@gmail.com>
2025-09-17 10:16:41 +0300

    Update c03/ex03

--- c03/ex03/ft_strncat.c
+++ c03/ex03/ft_strncat.c
@@ -1,20 +1,16 @@
+#include <stddef.h>
+
 char	*ft_strncat(char *dest, char *src, unsigned int nb)
 {
-	unsigned int	i;
-	unsigned int	j;
+	size_t	i;
+	size_t	j;
 
 	i = 0;
 	j = 0;
-	while (dest[i] != '\0')
-	{
+	while (dest[i])
 		i++;
-	}
-	while (src[j] != '\0' && j < nb)
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
+	while (src[j] && j < nb)
+		dest[i++] = src[j++];
 	dest[i] = '\0';
 	return (dest);
 }
