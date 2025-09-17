5ea8476362b1175bc748b3183a387dedfed1cba9
Mert Özcan <iammertozcan@gmail.com>
2025-09-17 10:16:51 +0300

    Update c03/ex04

--- c03/ex04/ft_strstr.c
+++ c03/ex04/ft_strstr.c
@@ -1,20 +1,22 @@
-char	*ft_strstr(char *str, char *to_find))
+#include <stddef.h>
+
+char	*ft_strstr(char *str, char *to_find)
 {
-	unsigned int	i;
-	unsigned int	j;
+	size_t	i;
+	size_t	j;
 
 	i = 0;
 	j = 0;
-	if (to_find[j] == '\0')
+	if (to_find[j])
 		return (str);
-	while (str[i] != '\0')
+	while (str[i])
 	{
-		while (str[i + j] == to_find[j] && str[i + j] != '\0')
+		while (str[i + j] == to_find[j] && str[i + j])
 			j++;
 		if (to_find[j] == '\0')
 			return (str + i);
 		i++;
 		j = 0;
 	}
-	return (0);
+	return (NULL);
 }
