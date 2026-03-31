ee147bcf64f46384ae4e2d077f198affc45d85e1
Mert Özcan <iammertozcan@gmail.com>
2026-03-31 23:33:33 +0300

    Add c06/ex03

---
+++ c06/ex03/ft_sort_params.c
@@ -0,0 +1,60 @@
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+		write(1, str++, 1);
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
+
+void	ft_sort_strings(int ac, char **av)
+{
+	int	i;
+	int	j;
+	char	*tmp;
+
+	i = 1;
+	while (i < ac - 1)
+	{
+		j = i + 1;
+		while (j < ac)
+		{
+			if (ft_strcmp(av[i], av[j]) > 0)
+			{
+				tmp = av[i];
+				av[i] = av[j];
+				av[j] = tmp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+void	ft_print_params(int ac, char **av)
+{
+	int	i;
+
+	i = 1;
+	while (i < ac)
+	{
+		ft_putstr(av[i]);
+		ft_putchar('\n');
+		i++;
+	}
+}
+
+void	ft_sort_params(int ac, char **av)
+{
+	ft_sort_strings(ac, av);
+	ft_print_params(ac, av);
+}
